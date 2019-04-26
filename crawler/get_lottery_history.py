8278edfadb08115fa477d5ed965718049f0132e9
rex5207 <abab5207@gmail.com>
2019-04-26 23:55:42 +0800

    update

--- crawler/get_lottery_history.py
+++ crawler/get_lottery_history.py
@@ -1,13 +1,6 @@
 from daily539 import Daily539
-from happy39M5 import Happy39
 
 if __name__ == "__main__":
     # daily539 = Daily539("top")
     # daily539.read_numbers_to_file()
     # daily539.dfs_all_numbers(0)
-
-    happy39 = Happy39()
-    # happy39.write_numbers_to_file(36)
-    happy39.read_numbers_to_file()
-    happy39.dfs_all_numbers(0)
-    happy39.show_top5()
